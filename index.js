aeea81f6d26b963e07e5199b1a9a8104fd64ad78
Sitong <nickren93@gmail.com>
2025-01-05 20:52:39 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        this.arrayOfSides = array
+    }
+
+    get countSides(){
+        return this.arrayOfSides.length
+    }
+
+    get perimeter(){
+        return this.arrayOfSides.reduce(this.calculatePerimeter, 0)
+    }
+
+    calculatePerimeter(accumulator, side){
+        return accumulator + side
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if (this.arrayOfSides[0] + this.arrayOfSides[1] > this.arrayOfSides[2] && 
+            this.arrayOfSides[0] + this.arrayOfSides[2] > this.arrayOfSides[1] &&
+            this.arrayOfSides[1] + this.arrayOfSides[2] > this.arrayOfSides[0]
+        ) {
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        if (this.arrayOfSides[0] === this.arrayOfSides[1] && 
+            this.arrayOfSides[0] ===  this.arrayOfSides[2] &&
+            this.arrayOfSides[0] === this.arrayOfSides[3]
+        ) {
+            return true
+        }else{
+            return false
+        }
+    }
+
+    get area(){
+        return this.arrayOfSides[0] * this.arrayOfSides[0]
+    }
+}
